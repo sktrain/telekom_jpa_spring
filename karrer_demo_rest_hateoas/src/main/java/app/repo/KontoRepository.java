3d3e3bb5e2d7578669e05974079e6e63aeff25f1
sktrain <39360116+sktrain@users.noreply.github.com>
2022-01-12 07:51:04 +0100

    Delete karrer_demo_rest_hateoas directory

--- karrer_demo_rest_hateoas/src/main/java/app/repo/KontoRepository.java
+++
@@ -1,9 +0,0 @@
-package app.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import app.model.Konto;
-
-public interface KontoRepository extends JpaRepository<Konto, Integer>{
-
-}
