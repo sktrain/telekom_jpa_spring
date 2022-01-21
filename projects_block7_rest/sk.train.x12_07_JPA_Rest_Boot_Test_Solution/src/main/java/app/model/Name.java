8f868a9f895d9daab35e875390f69b007d465ea9
sktrain <39360116+sktrain@users.noreply.github.com>
2022-01-21 08:48:19 +0100

    Add files via upload

---
+++ projects_block7_rest/sk.train.x12_07_JPA_Rest_Boot_Test_Solution/src/main/java/app/model/Name.java
@@ -0,0 +1,19 @@
+package app.model;
+
+public class Name {
+	
+	private String vorname;
+	private String nachname;
+	
+	public Name(String vorname, String nachname) {
+		super();
+		this.vorname = vorname;
+		this.nachname = nachname;
+	}
+
+	@Override
+	public String toString() {
+		return "Name [vorname=" + vorname + ", nachname=" + nachname + "]";
+	}	
+
+}
---
+++ projects_block7_rest/sk.train.x12_07_JPA_Rest_Boot_Test_Solution/src/main/java/app/web/EmployeeController.java
@@ -0,0 +1,73 @@
+package app.web;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.dao.EmpRepository;
+import app.model.Employee;
+
+@RestController
+@RequestMapping(path = "/employee")
+public class EmployeeController {
+
+	@Autowired
+	EmpRepository repo;
+
+	@GetMapping("/{id}")
+	public Employee empById(@PathVariable("id") Long id) {
+		Optional<Employee> optemp = repo.findById(id);
+		return optemp.orElseThrow(()-> new EmployeeNotFoundException("No Employee for id: " + id));
+	}
+
+	@GetMapping
+	public List<Employee> allEmps() {
+		return repo.findAll();
+	}
+
+	@PostMapping
+	public Employee saveEmp(@RequestBody Employee emp) {
+		if (repo.existsById(emp.getEmployeeId())) {
+			throw new EmployeeExistsException("Employee exists for id: " + emp.getEmployeeId());
+		}
+		return repo.save(emp);
+	}
+
+	@PutMapping("/{id}")
+	public Employee updateEmp(@PathVariable("id") Long id, @RequestBody Employee emp) {
+		if (repo.existsById(id)) {
+			return repo.save(emp);
+		} else {
+			throw new EmployeeNotFoundException("No Employee for id: "+ id);
+		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteEmp(@PathVariable("id") Long id) {
+		if (repo.existsById(id)) {
+			repo.deleteById(id);  //wirft nur IllegalArgumentException falls ID null
+		} else {
+			throw new EmployeeNotFoundException("No Employee for id: "+ id);
+		}
+	}
+
+	
+//	@ExceptionHandler(EmployeeExistsException.class)
+//	@ResponseStatus(HttpStatus.CONFLICT)
+//	public RestError employeeExistsHandler(EmployeeExistsException ex) {
+//		return new RestError(ex.getMessage(), 1);		
+//	}
+}
